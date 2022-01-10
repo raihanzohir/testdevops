544ceed36afc44f47fce3a6072847a940406781f
Md. Zohir Raihan <raihanzohir@users.noreply.github.com>
2022-01-11 00:31:13 +0600

    Update TestdevopsApplicationTests.java

--- src/test/java/com/simplilearn/devops/testdevops/TestdevopsApplicationTests.java
+++ src/test/java/com/simplilearn/devops/testdevops/TestdevopsApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class TestdevopsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
 }
